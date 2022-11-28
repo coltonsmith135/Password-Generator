740743545a7180cf89e0a49d437436424c2d0158
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2022-11-28 00:55:47 -0800

    added new code to my JavaScript script file to randomize my array

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var passwordLength = 128;
 var choiceArr = [];
-
+var password = [];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -9,6 +8,62 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+
+
+
+
+function generatePassword() {
+  
+  randomization.push();
+ 
+}
+
+function randomization() {
+
+  for(i = 0; i < choiceArr; i++ ) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length)
+    password = password + randomIndex;
+    password.push();
+  }
+  
+}
+
+
+function prompts () {
+  var passwordLength = parseInt(window.prompt("How many characters would you like your password to be?"));
+  
+
+  if (isNaN(passwordLength)) {
+      window.alert("Please enter a number.")
+     return
+  } 
+
+  if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("please enter a value between 8-128 characters.")
+     return
+  }
+
+  if (window.confirm("Would you to like include numbers?") === true){
+      choiceArr = choiceArr.concat(numbers);
+  }
+
+  if (window.confirm("Would you like to include symboles?") === true) {
+      choiceArr = choiceArr.concat(symbols);
+  }
+
+      if (window.confirm("Would you like to include lowercase letters?") === true) {
+      choiceArr = choiceArr.concat(lowercase);
+      }
+
+ if (window.confirm("Would you like to include uppercase letters?") === true) {
+     choiceArr = choiceArr.concat(uppercase);
+  }
+
+  
+}
+
 
 
 
@@ -29,51 +84,3 @@ function writePassword() {
    
 
 }
-
-
-function generatePassword() {
-  var password = "";
-  for(i = 0; i < passwordLength; i++ ) {
-    var randomIndex = Math.floor(Math.random() * choiceArr.length)
-    password = password + choiceArr[randomIndex];
-  }
-  return password;
-}
-
-function prompts () {
-  choiceArr = [];
-  var passwordLength = parseInt(window.prompt("How many characters would you like your password to be?"));
-  
-
-  if (isNaN(passwordLength)) {
-      window.alert("Please enter a number.")
-     return
-  } 
-
-  if (passwordLength < 8 || passwordLength > 128) {
-      window.alert("please enter a value between 8-128 characters.")
-     return
-  }
-
-  if (window.confirm("Would you to like include numbers?")){
-      choiceArr = choiceArr.concat(numbers);
-  }
-
-  if (window.confirm("Would you like to include symboles?")) {
-      choiceArr = choiceArr.concat(symbols);
-  }
-
-      if (window.confirm("Would you like to include lowercase letters?")) {
-      choiceArr = choiceArr.concat(lowercase);
-      }
-
- if (window.confirm("Would you like to include uppercase letters?"));{
-     choiceArr = choiceArr.concat(uppercase);
-  }
-
-  return true;
-}
-
-
-
-
